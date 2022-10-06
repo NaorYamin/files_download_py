a23418ffa462bc472f3ffa17bf99fa4224be2bf2
Yamin, Naor <naory902@gmail.com>
2022-10-06 11:23:33 +0300

    My Commit

---
+++ download_files.py
@@ -0,0 +1,37 @@
+import os
+import requests
+
+def download(url: str, dest_folder: str):
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)  # create folder if it does not exist
+
+    filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
+    file_path = os.path.join(dest_folder, filename)
+
+    r = requests.get(url, stream=True)
+    if r.ok:
+        print("saving to", os.path.abspath(file_path))
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+    else:  # HTTP status code 4XX/5XX
+        print("Download failed: status code {}\n{}".format(r.status_code, r.text))
+
+#This is where you can type your download link and destination folder, for example:
+download('https://download.anydesk.com/AnyDesk.exe', dest_folder=r'c:\temp')
+#You can also add more lines like this to download more files.
+
+
+
+
+
+
+
+
+
+
+
+
